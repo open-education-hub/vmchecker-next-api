72ce8bce43e0e156204051c4d58998676918a565
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-01-04 16:40:09 +0200

    docker changes

---
+++ bin/docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit 1
+
+docker run \
+  --restart always \
+  --name vmchecker-api \
+  --detach \
+  --env-file "($pwd)/../.env"
+  -p 8000:8000 \
+  jokeswar:vmchecker-api
