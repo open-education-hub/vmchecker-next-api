a00869ddca31b0aca046c4b089dffd3c9a123da4
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-01-04 16:48:29 +0200

    update entrypoint

--- bin/startapi.sh
+++ bin/startapi.sh
@@ -2,7 +2,8 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit 1
 
-uwsgi --chdir="$(pwd)" \
+./manage.py migrate
+exec uwsgi --chdir="$(pwd)" \
     --env DJANGO_SETTINGS_MODULE=api.settings \
     --http=0.0.0.0:8000 \
     --processes=5 \
