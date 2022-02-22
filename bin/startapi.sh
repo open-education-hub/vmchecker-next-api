8c4ba46899ab7f9ad00e4e70c3fb8253bccc06c2
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-02-22 12:12:53 +0200

    enable api task runner

--- bin/startapi.sh
+++ bin/startapi.sh
@@ -5,5 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit 1
 ./manage.py migrate
 exec uwsgi --chdir="$(pwd)" \
     --env DJANGO_SETTINGS_MODULE=api.settings \
+    --env API_TASK_RUNNER_ENABLED=True \
     --http=0.0.0.0:8000 \
     --ini etc/api.ini
