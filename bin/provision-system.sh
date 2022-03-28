edac69eb104af32834883c6ca45ee840a280639c
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2022-03-28 16:48:48 +0300

    Rename && Install dev packages in provision

--- bin/provision-system.sh
+++ bin/provision-system.sh
@@ -7,6 +7,6 @@ sudo apt-get install python3-pip
 
 pip3 install pipenv
 
-pipenv install --system --deploy --ignore-pipfile
+pipenv install --system --deploy --dev --ignore-pipfile
 
 pipenv run pytest
